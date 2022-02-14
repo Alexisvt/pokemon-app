95a00f0b2a93537e413f6c871515f3c4c05e420b
Alexis Villegas Torres <alexisvt@gmail.com>
2022-02-14 14:25:12 -0600

    Add components for product and part fo the website

--- tests/integration/components/cart-link-test.js
+++ tests/integration/components/cart-link-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | product', function (hooks) {
+module('Integration | Component | cart-link', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Product />`);
+    await render(hbs`<CartLink />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <Product>
+      <CartLink>
         template block text
-      </Product>
+      </CartLink>
     `);
 
     assert.dom(this.element).hasText('template block text');
